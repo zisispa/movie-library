f0b651351d6c7d08466c2854642d3952e8219a2f
Zisis Spatis <31807214+zisispa@users.noreply.github.com>
2020-12-18 13:23:42 +0200

    icons

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -51,7 +51,7 @@
                 <!-- Profile dropdown -->
                 <div class="ml-3 relative" x-data="{ isOpen: false }">
                     <div>
-                        <button @click="isOpen = !isOpen"
+                        <button @click="isOpen = !isOpen" @click.away="isOpen = false"
                             class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                             id="user-menu" aria-haspopup="true">
                             <span class="sr-only">Open user menu</span>
@@ -64,12 +64,12 @@
                         <div class="z-10 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
                             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                role="menuitem">Your
+                                role="menuitem"><i class="fas fa-user px-4"></i>Your
                                 Profile</a>
                             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                role="menuitem">Settings</a>
+                                role="menuitem"><i class="fas fa-heart px-4"></i>WatchList</a>
                             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                role="menuitem">Sign
+                                role="menuitem"><i class="fas fa-sign-out-alt px-4"></i>Sign
                                 out</a>
                         </div>
                     </template>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,11 +5,16 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title')</title>
+    <link rel="icon" href="{{ asset('img/popcorn.png') }}" type="image/x-icon">
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @livewireStyles
 
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+        crossorigin="anonymous" />
 </head>
 
 <body class="font-sans bg-gray-900 text-white">
